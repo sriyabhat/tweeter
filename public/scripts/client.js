2ffbb056147a606dc871b25484c329b28b8f982e
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-10-07 21:01:51 +0000

    Render an array of tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,25 +28,41 @@ const createTweetElement = (tweetObj) => {
   return output;
 };
 
+let data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+     },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweet = (tweets) => {
+  for(let tweetObj of tweets) {
+    const tweet = createTweetElement(tweetObj);
+    $('.tweets-container').append(tweet);
+  }
+}
+
 
 $(document).ready(function(){
-  
-  const obj = {
-         "user": {
-           "name": "Newton",
-           "avatars": "https://i.imgur.com/73hZDYK.png",
-           "handle": "@SirIsaac"
-         },
-         "content": {
-           "text": "If I have seen further it is by standing on the shoulders of giants"
-         },
-         "created_at": 1461116232227
-       };
-
-  const tweet = createTweetElement(obj);
-  console.log(tweet);
-  $('.tweets-container').append(tweet);
-  
+  renderTweet(data);
   $(".tweets").on('mouseover',function(event){
     $(event.currentTarget).children(".tweet-header").children("a").removeClass("hidden");    
   });
